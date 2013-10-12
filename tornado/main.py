d9730c934f8246a3e40959bcbc1b0ad15da4ee0f
Abhi Agarwal <me@abhiagarwal.com>
2013-10-12 17:46:58 -0400

    Adding Template

--- tornado/main.py
+++ tornado/main.py
@@ -1,29 +1,35 @@
+#!/usr/bin/env python
+import tornado.escape
 import tornado.ioloop
-from tornado.options import define, options
+import tornado.options
 import tornado.web
-import logging
+import tornado.websocket
+import os.path
+from tornado.options import define, options
 
 define("port", default = 8000, help = "run on the given port", type = int)
 
-settings = {
-    "debug": True,
-}
-
-server_settings = {
-    "xheaders" : True,
-}
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+        ]
+        settings = dict(
+            cookie_secret="8SGUe0QKS/ecvBl5WSYLw36RuNPtqEenqkIlAD0BoSY=",
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            xsrf_cookies=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello :)")
+        self.render("index.html")
 
 def main():
     tornado.options.parse_command_line()
-    logging.info("Starting Tornado web server on http://localhost:%s" % options.port)
-    application = tornado.web.Application([
-        (r"/", MainHandler),
-    ], **settings)
-    application.listen(options.port, **server_settings)
+    app = Application()
+    app.listen(options.port)
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
