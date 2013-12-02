f888663d521b39269b3222dff9e43686703f78d6
Abhi Agarwal <me@abhiagarwal.com>
2013-12-02 17:43:00 -0500

    Adding

--- tornado/static/to_js/app.js
+++ tornado/static/to_js/app.js
@@ -1,1 +1,1 @@
-$(window).load(function(){var e=$.url();if(e.param("type")!=undefined&&e.param("search")!=undefined){var t=e.param("type");var n=e.param("search");$("#tags_1").addTag(t);$("#tags_1").addTag(n)}else{$("#tags_1").addTag("gif")}})
+$(window).load(function(){var url=$.url();if(url.param("type")!=undefined&&url.param("search")!=undefined){var typeQuery=url.param("type");var searchQuery=url.param("search");$("#tags_1").addTag(typeQuery);$("#tags_1").addTag(searchQuery)}else{$("#tags_1").addTag("gif")}});
