656ab59ad35bde5bf350947d1a0c47835464df6f
Abhi Agarwal <me@abhiagarwal.com>
2013-12-05 14:36:31 -0500

    Adding files

--- tornado/mongo/analytics.py
+++ tornado/mongo/analytics.py
@@ -11,20 +11,32 @@ collection = db.timeout
 
 def averageTimeGIF(average, iterations):
 	GIFRankHandler = collection.find_one({"name": "GIFRankHandler"})
-	GIFRankHandler['avg'] = average
-	GIFRankHandler['iter'] = iterations
-	collection.save(GIFRankHandler)
+	if GIFRankHandler is None:
+		GIFRankHandler = {'avg': average, 'iter': iterations}
+		collection.insert(GIFRankHandler)
+	else:
+		GIFRankHandler['avg'] = average
+		GIFRankHandler['iter'] = iterations
+		collection.save(GIFRankHandler)
 
 def averageTimeNews(average, iterations):
 	NEWSRankHandler = collection.find_one({"name": "NEWSRankHandler"})
-	NEWSRankHandler['avg'] = average
-	NEWSRankHandler['iter'] = iterations
-	collection.save(NEWSRankHandler)
+	if NEWSRankHandler is None:
+		NEWSRankHandler = {'avg': average, 'iter': iterations}
+		collection.insert(NEWSRankHandler)
+	else:
+		NEWSRankHandler['avg'] = average
+		NEWSRankHandler['iter'] = iterations
+		collection.save(NEWSRankHandler)
 
 def getGIF():
 	GIFRankHandler = collection.find_one({"name": "GIFRankHandler"})
+	if GIFRankHandler is None:
+		return {'avg': 0.5, 'iter': 1}
 	return {'avg': GIFRankHandler['avg'], 'iter': GIFRankHandler['iter']}
 
 def getNews():
 	NEWSRankHandler = collection.find_one({"name": "NEWSRankHandler"})
+	if NEWSRankHandler is None:
+		return {'avg': 0.5, 'iter': 1}
 	return {'avg': NEWSRankHandler['avg'], 'iter': NEWSRankHandler['iter']}
