76cce27579997cdd1b9e3b13b2b20dd3f2ebcf65
Abhi Agarwal <me@abhiagarwal.com>
2013-12-08 02:41:13 -0500

    mongoexport

--- tornado/mongo/backup.py
+++ tornado/mongo/backup.py
@@ -1,7 +1,7 @@
 from subprocess import call
 
 def runbackup():
-    call(["mongodump", "-h widmore.mongohq.com:10010 -u tornado -ppT3mW49P81u -db Dubsit"])
+    call(["mongoexport", "-h widmore.mongohq.com:10010 -u tornado -ppT3mW49P81u -db Dubsit"])
 
 if __name__ == '__main__':
     runbackup()
