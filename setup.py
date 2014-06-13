63f52670dcbc6dbbbb87ddd42bbb437376732061
Abhi Agarwal <me@abhiagarwal.com>
2014-06-13 14:42:59 +0800

    Delete setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-sudo apt-get install git
-Y (to say yes)
-
-
-git config --global user.name "Abhi Agarwal"
-git config --global user.email me@abhiagarwal.com
-cd ~/.ssh
-ssh-keygen -t rsa -C "me@abhiagarwal.com"
