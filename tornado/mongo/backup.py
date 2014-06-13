2be004262787138cfe108861c5f78ccd05d38ab2
Abhi Agarwal <me@abhiagarwal.com>
2014-06-13 14:40:13 +0800

    Update backup.py

--- tornado/mongo/backup.py
+++ tornado/mongo/backup.py
@@ -1,7 +1,7 @@
 from subprocess import call
 
 def runbackup():
-    call(["mongoexport", "-h widmore.mongohq.com:10010 -u tornado -ppT3mW49P81u -db Dubsit"])
+    call(["mongoexport", "-h localhost -u -p -db "])
 
 if __name__ == '__main__':
     runbackup()
